d12b02ab945ee6664d19c14338cb9e8e436f4ac1
Vy Ho <hohoyvoh@gmail.com>
2025-05-17 12:52:19 +0700

    Splitted in 2 apis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os 
 import sys
-from fastapi import FastAPI, File, Form, HTTPException
+from fastapi import FastAPI, File, Form, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware  
 from typing import Optional
 from agent import Agent 
@@ -10,8 +10,12 @@ import logging
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 app = FastAPI() 
 agent = Agent() 
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,17 +25,63 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 
 @app.post("/api/v1/agent/get_text_response/", summary="Get response from agent")
+async def get_response(
+    conversation_id: str = Form(None),
+    user_id: str = Form(None),
+    text: str = Form(None),
+):
+
+    print(f"Received request with conversation_id: {conversation_id}, user_id: {user_id}, text: {text}")
+    try:
+        timestamp = datetime.datetime.now().isoformat()
+        input_data = {
+            "query": text,
+        }
+
+        response = await agent.get_response(
+            user_id=user_id,
+            input_data=input_data
+        )
+        
+        logger.info(f"Response from agent: {response}")
+
+        response_time = datetime.datetime.now()
+        response["latency"] = (response_time - datetime.datetime.fromisoformat(timestamp)).total_seconds()
+        return_data = {
+            "action": "get_response",
+            "status": "success",
+            "response": response,
+        }
+
+        return return_data
+
+    except ValueError as ve:
+        logger.error(f"Invalid input data: {str(ve)}")
+        raise HTTPException(status_code=400, detail={
+            "action": "get_response",
+            "status": "error",
+            "error": "Invalid input data",
+            "message": str(ve)
+        })
+    except Exception as e:
+        logger.error(f"Unexpected error: {str(e)}")
+        raise HTTPException(status_code=500, detail={
+            "action": "get_response",
+            "status": "error",
+            "error": "Internal server error",
+            "message": "An unexpected error occurred"
+        })
+    
+@app.post("/api/v1/agent/get_image_response/", summary="Get response from agent")
 async def get_response(
     conversation_id: str = Form(...),
     user_id: str = Form(...),
     text: str = Form(None),
-    image: Optional[bytes] = File(None),
-):  
+    image: Optional[UploadFile] = File(...),
+):
+
     print(f"Received request with conversation_id: {conversation_id}, user_id: {user_id}, text: {text}")
     try:
         timestamp = datetime.datetime.now().isoformat()
@@ -73,5 +123,3 @@ async def get_response(
             "error": "Internal server error",
             "message": "An unexpected error occurred"
         })
-    
-
